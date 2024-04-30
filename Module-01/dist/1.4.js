9ff4415d64cf21b974fd9dba8693860463e0985e
nabiur siddique <nabiursiddique01@gmail.com>
2024-04-30 22:36:33 +0600

    js file is generated after learning basic datatypes in ts

---
+++ Module-01/dist/1.4.js
@@ -0,0 +1,22 @@
+"use strict";
+//TODO: Basic Data Type
+//! primitive Data Types
+//* String
+let firstName = "Nabil";
+//* Number
+let roll = 123;
+//* Boolean
+let isAdmin = true;
+//* Undefined
+let x = undefined;
+//* null
+let y = null;
+let d;
+d = 123;
+//! Non-Primitive Data Types
+//* Array
+let friends = ["rohit", "utsab"];
+let rollList = [1, 2, 14];
+//* Tuple --> array --> order --> type of values
+let coordinates = [1, 5];
+let ageName = [24, "Nabil", true];
