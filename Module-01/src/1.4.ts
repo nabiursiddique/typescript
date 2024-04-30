fa8b92d1fbc5d0a9c24d958474db1dacf6edf191
nabiur siddique <nabiursiddique01@gmail.com>
2024-04-30 22:35:24 +0600

    Learned basic data types in typescript and how to write them

---
+++ Module-01/src/1.4.ts
@@ -0,0 +1,31 @@
+//TODO: Basic Data Type
+
+//! primitive Data Types
+
+//* String
+let firstName: string = "Nabil";
+
+//* Number
+let roll: number = 123;
+
+//* Boolean
+let isAdmin: boolean = true;
+
+//* Undefined
+let x: undefined = undefined;
+
+//* null
+let y: null = null;
+
+let d: number;
+d = 123;
+
+//! Non-Primitive Data Types
+
+//* Array
+let friends: string[] = ["rohit", "utsab"];
+let rollList: number[] = [1, 2, 14];
+
+//* Tuple --> array --> order --> type of values
+let coordinates: [number, number] = [1, 5];
+let ageName: [number, string, boolean] = [24, "Nabil", true];
