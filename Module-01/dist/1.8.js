7e754e369b3eed1bb56e6635c01f06650466e9a7
nabiur siddique <nabiursiddique01@gmail.com>
2024-05-01 11:24:09 +0600

    Learned destructuring in typescript

---
+++ Module-01/dist/1.8.js
@@ -0,0 +1,19 @@
+"use strict";
+{
+    // Destructuring
+    //* object destructuring (Explicit type declare not needed)
+    const user = {
+        id: 345,
+        name: {
+            firstName: "Nabiur",
+            middleName: "Siddique",
+            lastName: "Nabil",
+        },
+        contactNo: "01203721727",
+        address: "Naogaon",
+    };
+    const { contactNo, name: { middleName }, } = user;
+    //*  Array destructuring
+    const myFriends = ["Zisan", "Rohit", "Utsab", "Rafi", "Xhuvo"];
+    const [, , bestFriend, ...rest] = myFriends;
+}
