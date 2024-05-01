b0dfb137582f823e3e17c89f907b9960a5f1bee0
nabiur siddique <nabiursiddique01@gmail.com>
2024-05-01 12:51:52 +0600

    converted into js code

---
+++ Module-01/dist/1.9.js
@@ -0,0 +1,25 @@
+"use strict";
+{
+    const student1 = {
+        name: "Nabil",
+        age: 24,
+        gender: "Male",
+        contact: "01700000000",
+        address: "Naogaon",
+    };
+    const student2 = {
+        name: "Mir",
+        age: 35,
+        gender: "Male",
+        address: "Dhaka",
+    };
+    const student3 = {
+        name: "Mir",
+        age: 35,
+        gender: "Male",
+        address: "Dhaka",
+    };
+    const username = "Nabiur Siddique";
+    const isAdmin = true;
+    const add = (num1, num2) => num1 + num2;
+}
