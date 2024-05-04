065b265d092d8439a0a33bf3677bdbd4aeff0079
nabiur siddique <nabiursiddique01@gmail.com>
2024-05-04 22:48:27 +0600

    Type guards in ts(type guard, in guard).

---
+++ Module-03/3.3.ts
@@ -0,0 +1,50 @@
+{
+  //! Type guards
+
+  // typeof ----> type guard
+  type AlphaNumeric = string | number;
+
+  const add = (param1: AlphaNumeric, param2: AlphaNumeric): AlphaNumeric => {
+    if (typeof param1 === "number" && typeof param2 === "number") {
+      return param1 + param2;
+    } else {
+      return param1.toString() + param2.toString();
+    }
+  };
+  const result1 = add(2, 3);
+  const result2 = add("2", "3");
+  console.log(result1);
+  console.log(result2);
+
+  // typeof ----> in guard
+  type NormalUser = {
+    name: string;
+  };
+
+  type AdminUser = {
+    name: string;
+    role: "admin";
+  };
+
+  const getUser = (user: NormalUser | AdminUser) => {
+    if ("role" in user) {
+      console.log(`My name is ${user.name} and my role is ${user.role}`);
+    } else {
+      console.log(`My name is ${user.name}`);
+    }
+  };
+
+  const normalUser: NormalUser = {
+    name: "Mr.Normal Bhai",
+  };
+
+  const adminUser: AdminUser = {
+    name: "Mr.Admin Bhai",
+    role: "admin",
+  };
+
+  getUser(normalUser);
+  getUser(adminUser);
+
+  //
+}
