3f75fd038b680df8828c6f68ff8cbf3e26b9da1f
nabiur siddique <nabiursiddique01@gmail.com>
2024-05-04 19:59:54 +0600

    OOP- class in typescript

---
+++ Module-03/3.1.ts
@@ -0,0 +1,23 @@
+{
+  //! OOP- Class
+
+  class Animal {
+    constructor(
+      public name: string,
+      public species: string,
+      public sound: string
+    ) {}
+
+    makeSound() {
+      console.log(`The ${this.name} says ${this.sound}`);
+    }
+  }
+
+  const dog = new Animal("German Shepard", "dog", "Ghew Ghew");
+  const cat = new Animal("Persian", "cat", "Meow Meow");
+
+  dog.makeSound();
+  cat.makeSound();
+
+  //
+}
