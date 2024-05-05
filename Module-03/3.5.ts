fc00718a3bc32781e963d37701d2dd4a8209c49c
nabiur siddique <nabiursiddique01@gmail.com>
2024-05-05 09:36:31 +0600

    access modifiers(public,private,protected) in ts

---
+++ Module-03/3.5.ts
@@ -0,0 +1,38 @@
+{
+  //access modifiers
+  class BankAccount {
+    public readonly id: number;
+    public name: string;
+    // private _balance: number;
+    protected _balance: number;
+
+    constructor(id: number, name: string, balance: number) {
+      this.id = id;
+      this.name = name;
+      this._balance = balance;
+    }
+
+    public addDeposit(amount: number) {
+      this._balance = this._balance + amount;
+    }
+
+    public getBalance() {
+      return this._balance;
+    }
+  }
+
+  class StudentAccount extends BankAccount {
+    test() {
+      this._balance;
+    }
+  }
+
+  const goriberAccount = new BankAccount(111, "Mr.Gorib", 20);
+  goriberAccount.addDeposit(500);
+  const myBalance = goriberAccount.getBalance();
+  console.log(myBalance);
+
+  //
+}
+
+// ts-node-dev --respawn --transpile-only ./Module-03/3.5.ts
