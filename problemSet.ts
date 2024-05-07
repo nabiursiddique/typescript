d5cde0040b75bad711ae842a7534c7240af5f126
nabiur siddique <nabiursiddique01@gmail.com>
2024-05-07 17:52:21 +0600

    file edited

--- problemSet.ts
+++ problemSet.ts
@@ -1,4 +1,4 @@
-// //1. Create a function that takes an array of numbers as input and returns the sum of all the numbers in the array. Make sure to define the types for the input parameter and return value.
+// 1. Create a function that takes an array of numbers as input and returns the sum of all the numbers in the array. Make sure to define the types for the input parameter and return value.
 function sumArray(numbers: number[]): number {
   let sum = 0;
   for (let num of numbers) {
